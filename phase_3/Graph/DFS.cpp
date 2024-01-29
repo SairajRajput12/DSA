3f553d367643c0980a6e7477faac9bae626fc49a
SairajRajput12 <sairajrajput6@gmail.com>
2024-01-29 10:28:59 +0530

    29 Januvary 2024

--- phase_3/Graph/DFS.cpp
+++ phase_3/Graph/DFS.cpp
@@ -1,41 +1,48 @@
 #include<bits/stdc++.h> 
 using namespace std; 
 
-void dfs(int node,vector<int> adj[],int vis[],vector<int> &ls)
+void dfs(int node,vector<int> adj[],vector<int> &temp,int visited[])
 {
-    vis[node] = 1; 
-    ls.push_back(node); 
+    visited[node] = 1; 
+    temp.push_back(node); 
 
-    // traverse through each neighbour
-    for(auto it : adj[node]){
-        if(!vis[it]){
-            dfs(it,adj,vis,ls);
+    for(auto it : adj[node])
+    {
+        if(!visited[it]){
+            dfs(it,adj,temp,visited); 
         }
     }
 }
 
 vector<vector<int>> depthFirstSearch(int V, int E, vector<vector<int>> &edges)
 {
-    // Write your code here
+    // Declaration
+    vector<vector<int>> output; 
     vector<int> adj[V]; 
+    int visited[V] = {0}; // used to keep track of the visited or non visited node in the graph
+
+    // first create the graph 
     for(auto it : edges){
         adj[it[0]].push_back(it[1]); 
         adj[it[1]].push_back(it[0]); 
     }
 
-    int visited[V] = {0}; 
 
-    vector<vector<int>> output; 
-    for(int i = 0; i < V; i ++)
+
+    // now go through each edge and store the travesal of each edge
+    for(int i = 0;i < V; i ++)
     {
         if(!visited[i]){
-            vector<int> v; 
-            dfs(i,adj,visited,v); 
-            output.push_back(v); 
+            vector<int> temp; 
+            dfs(i,adj,temp,visited); 
+            output.push_back(temp);
         }
-    } 
+    }
+
+    // return the answer 2D vector
     return output; 
 }
+
 
 
 int main(){
