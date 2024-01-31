044032868a7505e5c7c96a5ad4d92774721896c8
SairajRajput12 <sairajrajput6@gmail.com>
2024-01-31 09:45:47 +0530

    31 Januvary 2024

--- AR/Assignment_1.cpp
+++ AR/Assignment_1.cpp
@@ -89,7 +89,6 @@ int main(){
         }
         cout << endl; 
      }
-
      
   return 0;
 }
---
+++ AR/Assignment_2.cpp
@@ -0,0 +1,38 @@
+#include<bits/stdc++.h>
+using namespace std;
+unordered_map<string,int> dictionary; 
+vector<int> Encode; 
+
+int main(){
+     // enter the project name 
+    cout << "I am batman " << endl;  
+
+    string input = "wabbabwabbabwabbabwabbabw"; 
+    string current = "";
+
+    for(int i = 0; i < input.size(); i ++){
+        char c = input[i]; 
+        string temp = c + current; 
+        if(dictionary.find(temp) != dictionary.end()){
+            // i got the string 
+            current = temp;    
+        }
+        else{
+            Encode.push_back(dictionary[current]); 
+            dictionary[temp] = dictionary.size(); 
+            current = (1,c); 
+        }
+    }
+
+    if(!current.empty()){
+        dictionary[current] = dictionary.size(); 
+    }
+
+    cout << "Encoded data : " << endl; 
+    for(int m : Encode){
+        cout << m << " "; 
+    }
+    cout << endl; 
+
+  return 0;
+}
