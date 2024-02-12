2f1c59e87d6c325a28a2a80b0b104f7a27abf91a
SairajRajput12 <sairajrajput6@gmail.com>
2024-02-12 16:40:03 +0530

    Using Kanhs Algorithm

--- phase_3/Graph/TopoSort.cpp
+++ phase_3/Graph/TopoSort.cpp
@@ -1,27 +1,7 @@
 #include<bits/stdc++.h>
-using namespace std; 
+using namespace std;
 
-class Solution {
-public:
-
-    bool dfs(int node,vector<int> adj[],int pathVisited[],int visited[]){
-        visited[node] = 1; 
-        pathVisited[node] = 1; 
-        
-        for(auto it : adj[node]){
-            if(!visited[it]){
-                if(dfs(it,adj,pathVisited,visited)){
-                    return true; 
-                }
-            }
-            else if(pathVisited[it]){
-                return true; 
-            }
-        }
-        pathVisited[node] = 0; 
-        return false; 
-    }
-
+#include <bits/stdc++.h> 
 
 
 void solve(int node,int visited[],stack<int> &st,vector<int> arr[]){
@@ -61,7 +41,7 @@ vector<int> topologicalSort(vector<vector<int>> &edges, int v, int e)  {
 
     // return ans; 
 
-    int visited[2000] = {0}; 
+    int visited[v] = {0}; 
     vector<int> ans; 
     stack<int> st; 
     vector<int> adj[v];
@@ -72,7 +52,7 @@ vector<int> topologicalSort(vector<vector<int>> &edges, int v, int e)  {
         adj[from].push_back(to); 
     }
 
-    int indegree[2000] = {0}; 
+    int indegree[v] = {0}; 
     for(int i = 0; i < v; i ++){
         for(auto it : adj[i]){
             indegree[it] ++; 
@@ -101,33 +81,6 @@ vector<int> topologicalSort(vector<vector<int>> &edges, int v, int e)  {
     return ans; 
 }
 
-    bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
-        // using the approach of cycle detection in the directed graph 
-        // int visited[2000] = {0}; 
-        // int pathVisited[2000] = {0};
-        // vector<int> adj[numCourses]; 
-
-        // for(auto it : prerequisites){
-        //     int from = it[0]; 
-        //     int to  = it[1]; 
-        //     adj[to].push_back(from); 
-        // }
-
-        // for(int i = 0; i < numCourses; i ++){
-        //     if(!visited[i]){
-        //         bool ans = dfs(i,adj,pathVisited,visited); 
-        //         if(ans){
-        //             return false; 
-        //         }
-        //     }
-        // }
-        // return true; 
-
-        // using kanh's algorithm: 
-        vector<int> ans = topologicalSort(prerequisites,numCourses,prerequisites.size()); 
-        return ans.size() == numCourses ? true : false; 
-    }
-};
 
 int main(){
      // enter the project name
